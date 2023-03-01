4ef1308298f204419ccbff008660ef3a5cc8c2fa
selvamanikandan S <40667407+selvamanikandans@users.noreply.github.com>
2023-03-01 15:51:26 +0530

    Update avalon.py

--- avalon.py
+++ avalon.py
@@ -20,7 +20,8 @@ import pymodbus
 import serial
 import time
 from pymodbus.pdu import ModbusRequest
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.client import ModbusSerialClient as ModbusClient 
+#from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import gpiod
 
 
